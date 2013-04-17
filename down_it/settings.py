a5ae265a6ad76a823d966494be31113cf5d7cbd3
Ricky Cook <mail@thatpanda.com>
2013-04-17 20:11:33 +1000

    Added the files_list app

--- down_it/settings.py
+++ down_it/settings.py
@@ -125,6 +125,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+
+    'files_list',
 )
 
 # A sample logging configuration. The only tangible logging
